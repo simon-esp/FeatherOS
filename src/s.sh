ad1a1f1789a5217d3e78055df3b1418ecda6e7b0
simon-esp <simon.esp1814@gmail.com>
2024-09-30 07:50:38 +0200

    Update s.sh

--- src/s.sh
+++ src/s.sh
@@ -1,3 +1,6 @@
 # setup.sh
-nasm -f bin boot.asm -o boot.bin
-qemu-system-x86_64 boot.bin
+nasm -f elf32 boot.asm -o boot_asm.o
+i386-elf-gcc -m16 -ffreestanding -c boot.c -o boot.o
+i386-elf-ld -Ttext 0x7C00 --oformat elf32-i386 -o boot.elf boot_asm.o boot.o
+i386-elf-objcopy -O binary boot.elf boot.bin
+qemu-system-x86_64 -drive format=raw,file=boot.bin
