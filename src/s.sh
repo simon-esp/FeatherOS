575f1b701a0f0cc96c6691714f698da82f405b06
simon-esp <simon.esp1814@gmail.com>
2024-09-30 14:07:56 +0200

    Update s.sh

--- src/s.sh
+++ src/s.sh
@@ -1,6 +1,43 @@
-# setup.sh
-nasm -f elf32 boot.asm -o boot_asm.o
-i386-elf-gcc -m16 -ffreestanding -c boot.c -o boot.o
-i386-elf-ld -Ttext 0x7C00 --oformat elf32-i386 -o boot.elf boot_asm.o boot.o
-i386-elf-objcopy -O binary boot.elf boot.bin
-qemu-system-x86_64 -drive format=raw,file=boot.bin
+#!/bin/bash
+
+# Set the output files
+BOOTLOADER_O="bootloader.o"
+BOOTLOADER_BIN="bootloader.bin"
+FLOPPY_IMG="floppy.img"
+
+# Remove previous output files if they exist
+rm -f $BOOTLOADER_O $BOOTLOADER_BIN $FLOPPY_IMG
+
+# Compile the bootloader
+echo "Compiling bootloader.c..."
+gcc -ffreestanding -c bootloader.c -o $BOOTLOADER_O
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to compile bootloader.c"
+    exit 1
+fi
+
+# Link the object file into a binary format
+echo "Linking bootloader..."
+ld -T linker.ld -o $BOOTLOADER_BIN $BOOTLOADER_O
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to link bootloader"
+    exit 1
+fi
+
+# Create a bootable floppy image
+echo "Creating floppy image..."
+dd if=/dev/zero of=$FLOPPY_IMG bs=512 count=2880
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to create floppy image"
+    exit 1
+fi
+
+dd if=$BOOTLOADER_BIN of=$FLOPPY_IMG conv=notrunc
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to write bootloader to floppy image"
+    exit 1
+fi
+
+# Run it with QEMU
+echo "Running bootloader in QEMU..."
+qemu-system-i386 -drive format=raw,file=$FLOPPY_IMG
